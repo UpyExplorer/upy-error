190afdf3fd83e26a7579e3d111f70becf93cfd9c
FernandoCelmer <email@fernandocelmer.com>
2022-07-22 23:56:03 -0300

    Revert "No Task - Implementing colors for exceptions"
    This reverts commit 5e49d5b04ef8151d08d8ed854e6c8bed76b2f8d0.

--- upy_error/base.py
+++ upy_error/base.py
@@ -7,34 +7,20 @@ Module Base
 import sys
 import traceback
 
-from .config import Style, logging, separator
-
-
-def format_exception(error: Exception, log: bool = False):
+def format_exception(error):
     """Return the exception in readable form
     Args:
         error (class): Exception
-        log (class): bool
     Returns:
         str: Traceback Error
     """
-
     exception_list = traceback.format_stack()
     exception_list = exception_list[:-2]
-    exception_list.extend(
-        traceback.format_tb(sys.exc_info()[2]))
-    exception_list.extend(
-        traceback.format_exception_only(sys.exc_info()[0], sys.exc_info()[1]))
+    exception_list.extend(traceback.format_tb(sys.exc_info()[2]))
+    exception_list.extend(traceback.format_exception_only(sys.exc_info()[0], sys.exc_info()[1]))
 
-    exception_str = "UpyError: \n"
+    exception_str = "Traceback (most recent call last):\n"
     exception_str += "".join(exception_list)
     exception_str = exception_str[:-1]
 
-    message = "\n" + Style.RED + exception_str + Style.WHITE + Style.END
-
-    if log:
-        separator()
-        logging.error(message)
-        separator()
-
-    return message
+    return exception_str
--- upy_error/config.py
+++ upy_error/config.py
@@ -6,17 +6,3 @@ logging.basicConfig(
     format='%(asctime)s %(levelname)s %(message)s',
     level=logging.WARN
 )
-
-
-class Style():
-    """
-    A simple color class
-    """
-
-    BLACK = '\033[30m'
-    RED = '\033[31m'
-    GREEN = '\033[32m'
-    YELLOW = '\033[33m'
-    BLUE = '\033[34m'
-    WHITE = '\033[37m'
-    END = '\033[0m'
